96e40c987a7f15f42def5e00d587ca96dbae34b9
Carter Beil <beilca@muohio.edu>
2012-09-04 22:35:36 -0400

    My background decided to change...
    I was working on trying to get my circle function to work, but my
    background decided to change instead... I'm not sure why..

--- src/CatPicturePart2App.cpp
+++ src/CatPicturePart2App.cpp
@@ -39,17 +39,30 @@ class CatPicturePart2App : public AppBasic {
 	static const int kAppHeight= 600;
 	static const int kTextureSize= 1024;
 
-	
+
+	//Made for smoother color changes
+	struct circle_signs{
+		int redSign;
+		int greenSign;
+		int blueSign;
+	};
+	//Going to use this to change my circle colors
+	struct circle_colors{
+		int r;
+		int g;
+		int b;
+	};
+
 	uint8_t* my_blur_pattern_;
 
 	//Filling a rectangle that will change colors
 	void drawRectangles(uint8_t* pixels, int x1, int y1, int x2, int y2);
 
-	void drawCircles(uint8_t* pixels, int x1, int y1, int r);
+	void drawBackground(uint8_t* pixels, int x1, int y1, int r);
 
 	void drawLine(uint8_t* pixels, int x1, int y1);
 
-	void drawBackground(uint8_t* pixels);
+	void drawCircles(uint8_t* pixels);
 
 };
 
@@ -61,9 +74,55 @@ void CatPicturePart2App::prepareSettings(Settings* settings)
 	(*settings).setResizable(true);
 }
 
-void CatPicturePart2App::drawCircles(uint8_t* pixels, int x, int y, int r)
+void CatPicturePart2App::drawBackground(uint8_t* pixels, int x, int y, int r)
 {
+	//Using these stucts causes the color changes in my background
+	circle_signs c;
+	c.redSign = 1;
+	c.greenSign = 1;
+	c.blueSign = 1;
 
+	circle_colors t;
+	t.r = 150;
+	t.g = 100;
+	t.b = 50;
+
+	//Declare new variables
+	int centerx = x;
+	int centery = y;
+	int radius = r;
+
+	//Checking boundaries
+	if (centerx < 0) return;
+	if (centery < 0) return;
+	if (radius <= 0) return;	
+
+	///I liked what you did in class, but I wanted my color transitions to be smoother
+	///So I made the colors bounce between the values of 0.0 - 1.0
+	if (red > 255 || red < 0) {
+		redSign = redSign*-1;
+	}
+	if (green > 255 || green < 0) {
+		greenSign = greenSign*-1;
+	}
+	if (blue > 255 || blue < 0) {
+		blueSign = blueSign*-1;
+	}
+
+	///Regardless of whether _Sign is (+/-) we will add it to the current values
+	red = red + (redSign);
+	green = green + (greenSign);
+	blue = blue + (blueSign);
+
+
+	for (int i = 0; i < 3*kTextureSize*kTextureSize; i+= 3){
+		int distance = (((i-x)*(i-x))+((i-y)*(i-y)));
+		if (distance <= r){
+			pixels[i] = red;
+			pixels[i+1] = green;
+			pixels[i+2] = blue;
+		}
+	}
 }
 
 void CatPicturePart2App::drawRectangles(uint8_t* pixels, int x1, int y1, int x2, int y2)
@@ -79,7 +138,7 @@ void CatPicturePart2App::drawRectangles(uint8_t* pixels, int x1, int y1, int x2,
 	//--------------------------------------------------
 
 	
-	//checking the boundary 
+	//checking the boundaries
 	if(endx < 0) return; 
 	if(endy < 0) return; 
 	if(startx >= kAppWidth) return;
@@ -106,16 +165,16 @@ void CatPicturePart2App::setup()
 	mySurface_ = new Surface(kTextureSize,kTextureSize,false);
 
 	//Set the initial values of the colors and color sign values
-	red = 0.0f;
-	green = 0.5f;
-	blue = 1.0f;
+	red = 25;
+	green = 155;
+	blue = 155;
 	redSign = 1;
 	greenSign = 1;
 	blueSign = 1;
 
 }
 
-void CatPicturePart2App::drawBackground(uint8_t* pixels)
+void CatPicturePart2App::drawCircles(uint8_t* pixels)
 {
 	///I liked what you did in class, but I wanted my color transitions to be smoother
 	///So I made the colors bounce between the values of 0.0 - 1.0
@@ -144,14 +203,15 @@ void CatPicturePart2App::drawBackground(uint8_t* pixels)
 
 void CatPicturePart2App::mouseDown( MouseEvent event )
 {
+
 }
 
 void CatPicturePart2App::update()
 {
 	uint8_t* dataArray = (*mySurface_).getData();
-	drawRectangles(dataArray, 250, 250, 300, 500);
-	drawCircles(dataArray, 500, 500, 500);
+	drawBackground(dataArray, 100, 200, 10);
 	//drawBackground(dataArray);
+	drawRectangles(dataArray, 250, 250, 300, 500);
 
 }
 
